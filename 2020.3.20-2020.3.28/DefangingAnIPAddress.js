d28ec84da2f5f9e09a6a851be53aabe91a9ce627
UntilGone <t_vampire@sina.cn>
2020-03-20 20:44:31 +0800

    fix: 移除错别字

--- 2020.3.20-2020.3.28/DefangingAnIPAddress.js
+++ 2020.3.20-2020.3.28/DefangingAnIPAddress.js
@@ -6,7 +6,7 @@
 const defangIPaddr = (add) => add.replace(/\./g, '[.]');
 
 // 总结
-// 这个就非常简单了，就是个就是个替换字符的操作
+// 这个就非常简单了，就是个替换字符的操作
 // 看了耗时比较短的，是通过split和join实现的
 // 不是很懂，为啥正则匹配会比数组操作慢,具体代码如下
 const defangIPaddrByArrayOperation = (add) => add.split('.').join('[.]');
